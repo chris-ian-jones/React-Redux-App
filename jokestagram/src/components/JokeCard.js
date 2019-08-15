d34570359e9d2de060189a50188991da7b285117
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-15 23:13:12 +0100

    Completed MVP: added censor to punchline of joke which gets removed when clicked

--- jokestagram/src/components/JokeCard.js
+++ jokestagram/src/components/JokeCard.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import styled from 'styled-components'
+
+const StyledText = styled.p`
+  font-size: 1.5rem;
+`
+
+const StyledCensor = styled.div`
+  :hover {
+    cursor: pointer;
+  }
+`
 
 const JokeCard = props => {
+  const [display, setDisplay] = useState('#545452')
+
   return (
     <Card>
       <Card.Content>
-        <Card.Header>{props.joke.setup}</Card.Header>
-        <Card.Description>{props.joke.punchline}</Card.Description>
+        <Card.Header>
+          <StyledText>{props.joke.setup}</StyledText>
+        </Card.Header>
+        <Card.Description>
+          <StyledCensor 
+            style={{backgroundColor: `${display}`, color: '#545452'}} 
+            onClick={() => setDisplay('transparent')}
+          >
+            <StyledText>{props.joke.punchline}</StyledText>
+          </StyledCensor>
+        </Card.Description>
       </Card.Content>
    </Card>
   )
